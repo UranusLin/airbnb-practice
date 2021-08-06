6977a98f815c56f6e1c7ea05a935aa0f30193c44
MorrisLin <aka611511@gmail.com>
2021-08-06 15:17:28 +0800

    day one build

---
+++ components/Footer.js
@@ -0,0 +1,41 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">About</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>It's a pretty awesome clone</p>
+                <p>Referrals accepted</p>
+                <p>MorrisLin</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>MorrisLin</p>
+                <p>Presents</p>
+                <p>Fork from papareact</p>
+                <p>This is text</p>
+                <p>This is text2</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help Centre</p>
+                <p>Trust & Safety</p>
+                <p>This a text</p>
+                <p>This a text2</p>
+                <p>This a text3</p>
+            </div>
+        </div>
+
+    )
+}
+
+export default Footer
